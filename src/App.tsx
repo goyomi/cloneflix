40c7657a4f7c423fc4f8a4483284bf7c4bad9c2f
goyomi <ghdl1004797@gmail.com>
2024-04-12 22:24:49 +0900

    Feat: Router setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Tv from "./routes/Tv";
+import Search from "./routes/Search";
+import Header from "./components/Header";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
