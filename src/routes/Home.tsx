86d4f88242bca24438b43076b879654c48eee181
goyomi <ghdl1004797@gmail.com>
2024-04-15 21:05:56 +0900

    Feat: 영화 메인 이미지가 포함된 배너 추가

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,59 @@
 import { useQuery } from "react-query";
 import { getMovies } from "../api";
 import { IGetMovies } from "../type";
+import styled from "styled-components";
+import { makeImagePath } from "../utils";
+
+const HomeContainer = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 10rem;
+`;
+
+const Banner = styled.article<{ bgPhoto: string }>`
+  height: 100vh;
+  padding: 6rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 5.4rem;
+  margin-bottom: 2rem;
+`;
+
+const Overview = styled.p`
+  width: 50%;
+  font-size: 1.6rem;
+  line-height: calc(1.6rem * 1.3);
+`;
 
 function Home() {
   const { data, isLoading } = useQuery<IGetMovies>(["movies", "nowPlaying"], getMovies);
   console.log(data, isLoading);
-  return <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>Home</div>;
+  return (
+    <HomeContainer>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </HomeContainer>
+  );
 }
 
 export default Home;
