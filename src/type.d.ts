b2545eb5173d063f349a273e095fb3853152d0c9
goyomi <ghdl1004797@gmail.com>
2024-04-23 17:44:08 +0900

    Refactor: 타입 선언을 'movies'에서 'movie'로 이름 변경

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,18 +1,19 @@
-export interface IGetMovies {
+export interface IGetMovie {
   dates: {
     maximum: string;
     minimum: string;
   };
   page: number;
-  results: IMovies[];
+  results: IMovie[];
   total_pages: number;
   total_results: number;
 }
 
-export interface IMovies {
+export interface IMovie {
   id: string;
   backdrop_path: string;
   poster_path: string;
-  title: string;
+  title?: string;
   overview: string;
+  name?: string;
 }
