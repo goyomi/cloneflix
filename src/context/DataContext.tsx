e080960d58e2cad5c46708dfcf4493142a506764
goyomi <ghdl1004797@gmail.com>
2024-04-24 17:18:22 +0900

    Feat: tv show DataContext/Provider 생성

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { createContext } from "react";
-import { getMovie } from "../api";
-import { IGetMovie } from "../type";
+import { getMovie, getTvShow } from "../api";
+import { IGetMovie, IGetTvShow } from "../type";
 
 interface IMovieDataContext {
   nowPlayingData: IGetMovie;
@@ -38,20 +38,84 @@ export const MovieDataContext = createContext<IMovieDataContext>({
 export const MovieProvider = () => {
   const { data: nowPlayingData, isLoading: nowPlayingIsLoading } = useQuery({
     queryKey: ["movie", "nowPlaying"],
-    queryFn: () => getMovie("now_playing"),
+    queryFn: () => getMovie("movie", "now_playing"),
   });
 
   const { data: topRatedData, isLoading: topRatedIsLoading } = useQuery({
     queryKey: ["movie", "topRated"],
-    queryFn: () => getMovie("top_rated"),
+    queryFn: () => getMovie("movie", "top_rated"),
   });
 
   const { data: upcomingData, isLoading: upcomingIsLoading } = useQuery({
     queryKey: ["movie", "upcoming"],
-    queryFn: () => getMovie("upcoming"),
+    queryFn: () => getMovie("movie", "upcoming"),
   });
 
   const isLoading = nowPlayingIsLoading || topRatedIsLoading || upcomingIsLoading;
 
   return { nowPlayingData, topRatedData, upcomingData, isLoading };
+};
+
+// TV show
+interface ITvShowDataContext {
+  airingTodayData: IGetTvShow;
+  onTheAirData: IGetTvShow;
+  popularData: IGetTvShow;
+  tvTopRatedData: IGetTvShow;
+  isLoading: boolean;
+}
+
+export const TvShowDataContext = createContext<ITvShowDataContext>({
+  airingTodayData: {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+  },
+  onTheAirData: {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+  },
+  popularData: {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+  },
+  tvTopRatedData: {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+  },
+  isLoading: true,
+});
+
+export const TvShowProvider = () => {
+  const { data: airingTodayData, isLoading: airingTodayIsLoading } = useQuery({
+    queryKey: ["tv", "airingToday"],
+    queryFn: () => getTvShow("tv", "airing_today"),
+  });
+
+  // Get a list of TV shows that air in the next 7 days
+  const { data: onTheAirData, isLoading: onTheAirIsLoading } = useQuery({
+    queryKey: ["tv", "onTheAir"],
+    queryFn: () => getTvShow("tv", "on_the_air"),
+  });
+
+  const { data: popularData, isLoading: popularIsLoading } = useQuery({
+    queryKey: ["tv", "popular"],
+    queryFn: () => getTvShow("tv", "popular"),
+  });
+
+  const { data: tvTopRatedData, isLoading: topRatedIsLoading } = useQuery({
+    queryKey: ["tv", "topRated"],
+    queryFn: () => getTvShow("tv", "top_rated"),
+  });
+
+  const isLoading = airingTodayIsLoading || onTheAirIsLoading || popularIsLoading || topRatedIsLoading;
+
+  return { airingTodayData, onTheAirData, popularData, tvTopRatedData, isLoading };
 };
