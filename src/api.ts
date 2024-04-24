5a65810f957f387a7f3e2f2a77a02bec9378f102
goyomi <ghdl1004797@gmail.com>
2024-04-24 17:12:49 +0900

    Refactor: tv show 데이터를 가져오는 함수 통합

--- src/api.ts
+++ src/api.ts
@@ -1,29 +1,13 @@
 const API_KEY = "3d6e29b3ff67795ee09d307f67541b45";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-export function getMovie(queryKey: string) {
-  return fetch(`${BASE_URL}/movie/${queryKey}?api_key=${API_KEY}&language=en-US&page=1
+export function getMovie(section: string, queryKey: string) {
+  return fetch(`${BASE_URL}/${section}/${queryKey}?api_key=${API_KEY}&language=en-US&page=1
   `).then((response) => response.json());
 }
 
-// TV Shows API
-export function getAiringTodayTVShow() {
-  return fetch(`${BASE_URL}/tv/airing_today?api_key=${API_KEY}&language=en-US&page=1`).then((response) =>
+export function getTvShow(section: string, queryKey: string) {
+  return fetch(`${BASE_URL}/${section}/${queryKey}?api_key=${API_KEY}&language=en-US&page=1`).then((response) =>
     response.json()
   );
-}
-
-// Get a list of TV shows that air in the next 7 days
-export function getOnTheAirTVShow() {
-  return fetch(`${BASE_URL}/tv/on_the_air?api_key=${API_KEY}&language=en-US&page=1`).then((response) =>
-    response.json()
-  );
-}
-
-export function getPopularTVShow() {
-  return fetch(`${BASE_URL}/tv/popular?api_key=${API_KEY}&language=en-US&page=1`).then((response) => response.json());
-}
-
-export function getTopRatedTVShow() {
-  return fetch(`${BASE_URL}/tv/top_rated?api_key=${API_KEY}&language=en-US&page=1`).then((response) => response.json());
 }
