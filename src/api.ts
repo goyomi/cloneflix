ed8787e9f1f5ff62c1499aa90c8b5be9be788911
goyomi <ghdl1004797@gmail.com>
2024-04-24 21:42:53 +0900

    Feat: detail 데이터를 가져오는 함수 추가

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,24 @@
 const API_KEY = "3d6e29b3ff67795ee09d307f67541b45";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-export function getMovie(section: string, queryKey: string) {
-  return fetch(`${BASE_URL}/${section}/${queryKey}?api_key=${API_KEY}&language=en-US&page=1
+export function getMovie(section: string, category: string) {
+  return fetch(`${BASE_URL}/${section}/${category}?api_key=${API_KEY}&language=en-US&page=1
   `).then((response) => response.json());
 }
 
-export function getTvShow(section: string, queryKey: string) {
-  return fetch(`${BASE_URL}/${section}/${queryKey}?api_key=${API_KEY}&language=en-US&page=1`).then((response) =>
+export function getMovieDetail(section: string, id: string) {
+  return fetch(`${BASE_URL}/${section}/${id}?api_key=${API_KEY}&language=en-US&page=1
+  `).then((response) => response.json());
+}
+
+export function getTvShow(section: string, category: string) {
+  return fetch(`${BASE_URL}/${section}/${category}?api_key=${API_KEY}&language=en-US&page=1`).then((response) =>
+    response.json()
+  );
+}
+
+export function getTvShowDetail(section: string, id: string) {
+  return fetch(`${BASE_URL}/${section}/${id}?api_key=${API_KEY}&language=en-US&page=1`).then((response) =>
     response.json()
   );
 }
