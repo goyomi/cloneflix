8828765ead601d1a567f8a1d5134070afc22046b
goyomi <ghdl1004797@gmail.com>
2024-04-25 22:18:21 +0900

    Rename, Feat: 파일 확장자를 .ts에서 .tsx로 변경, 별점을 계산하는 유틸리티 함수 추가

--- src/utils.ts
+++
@@ -1,3 +0,0 @@
-export function makeImagePath(id: string, width?: string) {
-  return `https://image.tmdb.org/t/p/${width ? width : "original"}/${id}`;
-}
---
+++ src/utils.tsx
@@ -0,0 +1,29 @@
+export function makeImagePath(id: string, width?: string) {
+  return `https://image.tmdb.org/t/p/${width ? width : "original"}/${id}`;
+}
+
+export function starRating(vote_average: number) {
+  const fullStar = Math.floor(vote_average / 2);
+  const halfStar = vote_average % 2 >= 0.5 ? 1 : 0;
+  const emptyStar = 5 - fullStar - halfStar;
+
+  return (
+    <>
+      {Array(fullStar)
+        .fill(0)
+        .map((_, idx) => (
+          <span key={idx} className="full">
+            ★
+          </span>
+        ))}
+      {halfStar > 0 && <span className="half">★</span>}
+      {Array(emptyStar)
+        .fill(0)
+        .map((_, idx) => (
+          <span key={idx} className="empty">
+            ★
+          </span>
+        ))}
+    </>
+  );
+}
