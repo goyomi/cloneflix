dae92b91b0b673bcf585c0945882c7ee75e90896
goyomi <ghdl1004797@gmail.com>
2024-05-01 23:41:24 +0900

    Feat, Rename: Home페이지 추가 및 Movie 페이지 이름 변경으로 route 추가 및 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./routes/Home";
 import Tv from "./routes/Tv";
 import Search from "./routes/Search";
 import Header from "./components/Header";
+import Movie from "./routes/Movie";
 
 function App() {
   return (
@@ -12,7 +13,10 @@ function App() {
         <Route path="/search" element={<Search />} />
         <Route path="/tv/:category/:tvId" element={<Tv />} />
         <Route path="/tv" element={<Tv />} />
-        <Route path="/movie/:category/:movieId" element={<Home />} />
+        <Route path="/movie/:category/:movieId" element={<Movie />} />
+        <Route path="/movie" element={<Movie />} />
+        <Route path="/tv/trending/:tvId" element={<Home />} />
+        <Route path="/movie/trending/:movieId" element={<Home />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
