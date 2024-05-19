426ce056d3c09f6ea25df8442fbcd7b3c8726bfc
goyomi <ghdl1004797@gmail.com>
2024-05-19 16:30:36 +0900

    Design: similar content UI 변경

--- src/components/ModalContents/SimilarContent.tsx
+++ src/components/ModalContents/SimilarContent.tsx
@@ -4,22 +4,50 @@ import { makeImagePath } from "../../utils";
 import { CreditWrapper, MemberCard, MemberWrapper } from "./CastMember";
 
 const SimilarWrapper = styled(CreditWrapper)``;
+const ContentWrapper = styled(MemberWrapper)`
+  grid-template-columns: repeat(4, 1fr);
+`;
+const ContentCard = styled(MemberCard)`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 0.5rem;
+  .title {
+    font-size: 1.7rem;
+  }
+  .text_wrapper {
+    display: flex;
+    justify-content: space-between;
+    .date,
+    .vote_average {
+      font-size: 1.4rem;
+    }
+    .vote_average {
+      color: gold;
+    }
+  }
+`;
 
 function SimilarContent({ similarData, section }: { similarData: ISimilar; section: string }) {
+  console.log(similarData.results);
   return (
     <SimilarWrapper>
       <h2 className="title">Similar {section}</h2>
-      <MemberWrapper>
+      <ContentWrapper>
         {similarData.results.slice(0, 6).map((data, idx) => (
-          <MemberCard key={idx}>
-            <div className="member_image" style={{ backgroundImage: `url(${makeImagePath(data.poster_path)})` }}>
+          <ContentCard key={idx}>
+            <div className="content_image" style={{ backgroundImage: `url(${makeImagePath(data.poster_path)})` }}>
               {!data.poster_path && "No Image"}
             </div>
-            <span>{data.name || data.title}</span>
-            <span>(★ {data.vote_average})</span>
-          </MemberCard>
+            <span className="title">{data.name || data.title}</span>
+            <div className="text_wrapper">
+              <span className="date">{data.release_date || data.first_air_date}</span>
+              <span className="vote_average">★ {data.vote_average}</span>
+            </div>
+          </ContentCard>
         ))}
-      </MemberWrapper>
+      </ContentWrapper>
     </SimilarWrapper>
   );
 }
