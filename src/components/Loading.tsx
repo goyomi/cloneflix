614050a18f3d1e8cf240441d8ed6f37b07416b82
goyomi <ghdl1004797@gmail.com>
2024-06-07 00:08:42 +0900

    Add, Feat: Loading 컴포넌트 파일 추가, Loading UI 및 스피너 로직 구현

---
+++ src/components/Loading.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  span {
+    font-size: 5rem;
+  }
+`;
+
+function Loading() {
+  const [dots, setDots] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prevDots) => {
+        if (prevDots.length < 3) {
+          return prevDots + ".";
+        } else {
+          return "";
+        }
+      });
+    }, 500);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <Container>
+      <span>Loading{dots}</span>
+    </Container>
+  );
+}
+
+export default Loading;
