714acdd058ce7e674e5d9bad107d30878a629505
goyomi <ghdl1004797@gmail.com>
2024-06-07 23:59:31 +0900

    Refactor: 공통 스타일 컴포넌트를 export로 내보내기

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
+export const Container = styled.div`
   height: 100vh;
   display: flex;
   align-items: center;
